270d81d099a06ddce3e814dd928ee6d80e3b7ecd
Gordondalos <gordondalos@gmail.com>
2016-09-23 22:06:41 +0600

    Реализовал добавление групп

---
+++ src/UserBundle/Controller/GroupController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use UserBundle\Entity\Group;
+use UserBundle\Form\GroupType;
+
+/**
+ * Group controller.
+ *
+ */
+class GroupController extends Controller
+{
+    /**
+     * Lists all Group entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $groups = $em->getRepository('UserBundle:Group')->findAll();
+
+        return $this->render('group/index.html.twig', array(
+            'groups' => $groups,
+        ));
+    }
+
+    /**
+     * Creates a new Group entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $group = new Group();
+        $form = $this->createForm('UserBundle\Form\GroupType', $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($group);
+            $em->flush();
+
+            return $this->redirectToRoute('group_show', array('id' => $group->getId()));
+        }
+
+        return $this->render('group/new.html.twig', array(
+            'group' => $group,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Group entity.
+     *
+     */
+    public function showAction(Group $group)
+    {
+        $deleteForm = $this->createDeleteForm($group);
+
+        return $this->render('group/show.html.twig', array(
+            'group' => $group,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Group entity.
+     *
+     */
+    public function editAction(Request $request, Group $group)
+    {
+        $deleteForm = $this->createDeleteForm($group);
+        $editForm = $this->createForm('UserBundle\Form\GroupType', $group);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($group);
+            $em->flush();
+
+            return $this->redirectToRoute('group_edit', array('id' => $group->getId()));
+        }
+
+        return $this->render('group/edit.html.twig', array(
+            'group' => $group,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Group entity.
+     *
+     */
+    public function deleteAction(Request $request, Group $group)
+    {
+        $form = $this->createDeleteForm($group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($group);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('group_index');
+    }
+
+    /**
+     * Creates a form to delete a Group entity.
+     *
+     * @param Group $group The Group entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Group $group)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('group_delete', array('id' => $group->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
